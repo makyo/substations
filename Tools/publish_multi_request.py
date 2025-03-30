e27911c2436dad1b0e1e579e803fa917c63f202c
Madison Scott-Clary <makyo@drab-makyo.com>
2025-03-29 23:40:42 -0700

    Prepare for CDN

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -14,8 +14,8 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://cdn.deltav.gay/"
-FORK_ID = "delta-v"
+ROBUST_CDN_URL = "https://cdn.lagrange14.com/"
+FORK_ID = "substations"
 
 def main():
     session = requests.Session()
