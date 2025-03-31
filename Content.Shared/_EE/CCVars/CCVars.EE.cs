adb3fc7771dfdd8fc8d82363d5153e09ef501243
Madison Scott-Clary <makyo@drab-makyo.com>
2025-03-31 09:42:05 -0700

    Move added EE content to _EE where possible, comment where not.

--- Content.Shared/_EE/CCVars/CCVars.EE.cs
+++ Content.Shared/_EE/CCVars/CCVars.EE.cs
@@ -5,10 +5,15 @@ namespace Content.Shared._EE.CCVars;
 [CVarDefs]
 public sealed partial class EECVars
 {
-
     /// <summary>
-    ///     EE: Is ore material enabled.
+    ///     Whether or not the materials silo is enabled.
     /// </summary>
     public static readonly CVarDef<bool> SiloEnabled =
         CVarDef.Create("silo.silo_enabled", true, CVar.SERVER | CVar.REPLICATED);
+
+    /// <summary>
+    ///     How many lines back in the chat log to look for collapsing repeated messages into one.
+    /// </summary>
+    public static readonly CVarDef<int> ChatStackLastLines =
+        CVarDef.Create("chat.chatstack_last_lines", 1, CVar.CLIENTONLY | CVar.ARCHIVE, "How far into the chat history to look when looking for similiar messages to coalesce them.");
 }
