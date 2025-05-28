ee7c12e946b8d24f83e202acc6e9be6669220dca
deltanedas <39013340+deltanedas@users.noreply.github.com>
2025-05-28 12:52:11 -0700

    action refactor proper ecs edition (#27422)
    (cherry picked from commit b3825dce97e7debd8ea376ce7f247e050f812eb2)

--- Content.Shared/RatKing/SharedRatKingSystem.cs
+++ Content.Shared/RatKing/SharedRatKingSystem.cs
@@ -1,5 +1,5 @@
-﻿using Content.Shared.Abilities;
 using Content.Shared.Actions;
+﻿using Content.Shared.Actions.Components;
 using Content.Shared.DoAfter;
 using Content.Shared.Random;
 using Content.Shared.Random.Helpers;
@@ -10,13 +10,11 @@ using Robust.Shared.Network;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
 using Robust.Shared.Serialization;
-using Robust.Shared.Timing;
 
 namespace Content.Shared.RatKing;
 
 public abstract class SharedRatKingSystem : EntitySystem
 {
-    [Dependency] private readonly IGameTiming _gameTiming = default!; // Used for rummage cooldown
     [Dependency] private readonly INetManager _net = default!;
     [Dependency] protected readonly IPrototypeManager PrototypeManager = default!;
     [Dependency] protected readonly IRobustRandom Random = default!;
@@ -63,12 +61,13 @@ public abstract class SharedRatKingSystem : EntitySystem
         if (!TryComp(uid, out ActionsComponent? comp))
             return;
 
-        _action.RemoveAction(uid, component.ActionRaiseArmyEntity, comp);
-        _action.RemoveAction(uid, component.ActionDomainEntity, comp);
-        _action.RemoveAction(uid, component.ActionOrderStayEntity, comp);
-        _action.RemoveAction(uid, component.ActionOrderFollowEntity, comp);
-        _action.RemoveAction(uid, component.ActionOrderCheeseEmEntity, comp);
-        _action.RemoveAction(uid, component.ActionOrderLooseEntity, comp);
+        var actions = new Entity<ActionsComponent?>(uid, comp);
+        _action.RemoveAction(actions, component.ActionRaiseArmyEntity);
+        _action.RemoveAction(actions, component.ActionDomainEntity);
+        _action.RemoveAction(actions, component.ActionOrderStayEntity);
+        _action.RemoveAction(actions, component.ActionOrderFollowEntity);
+        _action.RemoveAction(actions, component.ActionOrderCheeseEmEntity);
+        _action.RemoveAction(actions, component.ActionOrderLooseEntity);
     }
 
     private void OnOrderAction(EntityUid uid, RatKingComponent component, RatKingOrderActionEvent args)
@@ -108,12 +107,7 @@ public abstract class SharedRatKingSystem : EntitySystem
 
     private void OnGetVerb(EntityUid uid, RatKingRummageableComponent component, GetVerbsEvent<AlternativeVerb> args)
     {
-        if (!HasComp<RummagerComponent>(args.User)
-            || component.Looted
-            || _gameTiming.CurTime < component.LastLooted + component.RummageCooldown)
-            // DeltaV - Use RummagerComponent instead of RatKingComponent
-            // (This is so we can give Rodentia rummage abilities)
-            // Additionally, adds a cooldown check
+        if (!HasComp<RatKingComponent>(args.User) || component.Looted)
             return;
 
         args.Verbs.Add(new AlternativeVerb
@@ -136,18 +130,10 @@ public abstract class SharedRatKingSystem : EntitySystem
 
     private void OnDoAfterComplete(EntityUid uid, RatKingRummageableComponent component, RatKingRummageDoAfterEvent args)
     {
-        // DeltaV - Rummaging an object updates the looting cooldown rather than a "previously looted" check.
-        // Note that the "Looted" boolean can still be checked (by mappers/admins) 
-        // to disable rummaging on the object indefinitely, but rummaging will no
-        // longer permanently prevent future rummaging.
-        var time = _gameTiming.CurTime;
-        if (args.Cancelled
-            || component.Looted
-            || time < component.LastLooted + component.RummageCooldown)
+        if (args.Cancelled || component.Looted)
             return;
 
-        component.LastLooted = time;
-        // End DeltaV change
+        component.Looted = true;
         Dirty(uid, component);
         _audio.PlayPredicted(component.Sound, uid, args.User);
 
