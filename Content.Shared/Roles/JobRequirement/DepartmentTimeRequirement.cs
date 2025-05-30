c3b5a608801774ba6cd57952589441efe4e6cdac
Perry Fraser <perryprog@users.noreply.github.com>
2025-05-30 14:07:51 -0400

    feat: disable time requirements for loadouts (#288)

--- Content.Shared/Roles/JobRequirement/DepartmentTimeRequirement.cs
+++ Content.Shared/Roles/JobRequirement/DepartmentTimeRequirement.cs
@@ -32,6 +32,8 @@ public sealed partial class DepartmentTimeRequirement : JobRequirement
         bool isWhitelisted) // DeltaV
     {
         reason = new FormattedMessage();
+        return true; // L5 - lol. (Even though game.role_timers exists, loadout effects ignore it.)
+        /*
         var playtime = TimeSpan.Zero;
 
         // Check all jobs' departments
@@ -83,5 +85,6 @@ public sealed partial class DepartmentTimeRequirement : JobRequirement
         }
 
         return true;
+        */
     }
 }
