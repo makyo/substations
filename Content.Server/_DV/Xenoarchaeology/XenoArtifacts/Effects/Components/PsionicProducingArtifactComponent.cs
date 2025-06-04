aa5588dde0e4315eb554e6b28c23c062daebb58d
Madison Rye Progress <makyo@drab-makyo.com>
2025-06-04 13:42:32 -0700

    Cherry-pick new xenoarch
    Also involves getting rid of psionic effects and triggers, plus an additional action from Imp

--- Content.Server/_DV/Xenoarchaeology/XenoArtifacts/Effects/Components/PsionicProducingArtifactComponent.cs
+++ Content.Server/_DV/Xenoarchaeology/XenoArtifacts/Effects/Components/PsionicProducingArtifactComponent.cs
@@ -1,22 +1,24 @@
-using Content.Server._DV.Xenoarchaeology.XenoArtifacts.Effects.Systems;
+// L5 — Removing psionics, switching to new xenoarch
 
-namespace Content.Server._DV.Xenoarchaeology.XenoArtifacts.Effects.Components;
-
-/// <summary>
-/// Makes people in a radius around it psionic when triggered.
-/// </summary>
-[RegisterComponent, Access(typeof(PsionicProducingArtifactSystem))]
-public sealed partial class PsionicProducingArtifactComponent : Component
-{
-    /// <summary>
-    /// Range to look for potential psionics in.
-    /// </summary>
-    [DataField(required: true)]
-    public float Range;
-
-    /// <summary>
-    /// Number of times this node can trigger before it switches to doing nothing.
-    /// </summary>
-    [DataField]
-    public int Limit = 1;
-}
+// using Content.Server._DV.Xenoarchaeology.XenoArtifacts.Effects.Systems;
+//
+// namespace Content.Server._DV.Xenoarchaeology.XenoArtifacts.Effects.Components;
+//
+// /// <summary>
+// /// Makes people in a radius around it psionic when triggered.
+// /// </summary>
+// [RegisterComponent, Access(typeof(PsionicProducingArtifactSystem))]
+// public sealed partial class PsionicProducingArtifactComponent : Component
+// {
+//     /// <summary>
+//     /// Range to look for potential psionics in.
+//     /// </summary>
+//     [DataField(required: true)]
+//     public float Range;
+//
+//     /// <summary>
+//     /// Number of times this node can trigger before it switches to doing nothing.
+//     /// </summary>
+//     [DataField]
+//     public int Limit = 1;
+// }
