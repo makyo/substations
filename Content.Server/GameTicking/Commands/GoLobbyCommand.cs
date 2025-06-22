401a62ecc04ea4718bfd5fd170be6d5faa285f0c
Perry Fraser <perryprog@users.noreply.github.com>
2025-06-22 15:38:18 -0400

    feat: add confirm to /golobby on > 1 hour rounds (#360)

--- Content.Server/GameTicking/Commands/GoLobbyCommand.cs
+++ Content.Server/GameTicking/Commands/GoLobbyCommand.cs
@@ -4,6 +4,7 @@ using Content.Shared.Administration;
 using Content.Shared.CCVar;
 using Robust.Shared.Configuration;
 using Robust.Shared.Console;
+using Robust.Shared.Timing;
 
 namespace Content.Server.GameTicking.Commands
 {
@@ -11,18 +12,31 @@ namespace Content.Server.GameTicking.Commands
     public sealed class GoLobbyCommand : IConsoleCommand
     {
         [Dependency] private readonly IEntityManager _e = default!;
+        [Dependency] private readonly IGameTiming _time = default!; // L5
 
         public string Command => "golobby";
         public string Description => "Enables the lobby and restarts the round.";
-        public string Help => $"Usage: {Command} / {Command} <preset>";
+        // L5 - add confirm
+        public string Help => $"Usage: {Command} [confirm] / {Command} <preset> [confirm]";
+
         public void Execute(IConsoleShell shell, string argStr, string[] args)
         {
             GamePresetPrototype? preset = null;
             var presetName = string.Join(" ", args);
+            // Begin L5 changes - add confirm
+            var confirm = presetName.EndsWith("confirm");
+            if (confirm)
+                presetName = presetName[..^"confirm".Length].TrimEnd();
+            else if (_time.RealTime > TimeSpan.FromHours(1))
+            {
+                shell.WriteLine($"Add 'confirm' to the command to really end the round and go back to the lobby.");
+                return;
+            }
+            // End L5 changes
 
             var ticker = _e.System<GameTicker>();
 
-            if (args.Length > 0)
+            if (presetName.Length > 0) // L5 - was args.Length
             {
                 if (!ticker.TryFindGamePreset(presetName, out preset))
                 {
