74518773354d853eaceb0a28dc7b155f9832344f
Perry Fraser <perry@frasers.org>
2025-07-09 14:18:24 -0400

    fix: reimplement golobby's confirm

--- Content.Server/GameTicking/Commands/GoLobbyCommand.cs
+++ Content.Server/GameTicking/Commands/GoLobbyCommand.cs
@@ -4,6 +4,7 @@ using Content.Shared.Administration;
 using Content.Shared.CCVar;
 using Robust.Shared.Configuration;
 using Robust.Shared.Console;
+using Robust.Shared.Timing;
 
 namespace Content.Server.GameTicking.Commands
 {
@@ -12,6 +13,7 @@ namespace Content.Server.GameTicking.Commands
     {
         [Dependency] private readonly IConfigurationManager _configManager = default!;
         [Dependency] private readonly GameTicker _gameTicker = default!;
+        [Dependency] private readonly IGameTiming _time = default!; // L5
 
         public override string Command => "golobby";
 
@@ -19,8 +21,18 @@ namespace Content.Server.GameTicking.Commands
         {
             GamePresetPrototype? preset = null;
             var presetName = string.Join(" ", args);
+            // Begin L5 changes - add confirm
+            var confirm = presetName.EndsWith("confirm");
+            if (confirm)
+                presetName = presetName[..^"confirm".Length].TrimEnd();
+            else if (_time.RealTime > TimeSpan.FromHours(1))
+            {
+                shell.WriteLine(Loc.GetString("cmd-golobby-needs-confirm"));
+                return;
+            }
+            // End L5 changes
 
-            if (args.Length > 0)
+            if (presetName.Length > 0) // L5 - was args.Length
             {
                 if (!_gameTicker.TryFindGamePreset(presetName, out preset))
                 {
