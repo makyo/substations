teecoding/Zona-14

Command resolve mega pr batch 5 (#38389)
* commit progress

* requested changes

Resources/Locale/en-US/administration/commands/call-shuttle-command.ftl
@@ -1,4 +1,4 @@
-call-shuttle-command-description = Calls the emergency shuttle with an optionally provided arrival time.
-call-shuttle-command-help-text = Usage: {$command} [m:ss]
-recall-shuttle-command-description = Recalls the emergency shuttle.
-recall-shuttle-command-help-text = Usage: {$command}
+cmd-callshuttle-desc = Calls the emergency shuttle with an optionally provided arrival time.
+cmd-callshuttle-help = Usage: callshuttle [m:ss]
+cmd-recallshuttle-desc = Recalls the emergency shuttle.
+cmd-recallshuttle-help = Usage: recallshuttle

makyo/substations

(cherry picked from commit 3485450ffa8e4d60cde8da4c31a964d1d0eeca0f)

Resources/Locale/en-US/administration/commands/call-shuttle-command.ftl
@@ -1,6 +1,6 @@
 # L5
-call-shuttle-command-description = Calls the evacuation shuttle with an optionally provided arrival time.
-call-shuttle-command-help-text = Usage: {$command} [m:ss]
+cmd-callshuttle-desc = Calls the evacuation shuttle with an optionally provided arrival time.
+cmd-callshuttle-help = Usage: callshuttle [m:ss]
 # L5
-recall-shuttle-command-description = Recalls the evacuation shuttle.
-recall-shuttle-command-help-text = Usage: {$command}
+cmd-recallshuttle-desc = Recalls the evacuation shuttle.
+cmd-recallshuttle-help = Usage: recallshuttle