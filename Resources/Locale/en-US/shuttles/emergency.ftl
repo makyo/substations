RebelledCombine/Zona-14

Revisions and cleanup to dock and shuttle commands. (#38533)
commit

Resources/Locale/en-US/shuttles/emergency.ftl
@@ -1,14 +1,17 @@
 # Commands
 ## Delay shuttle round end
-emergency-shuttle-command-round-desc = Stops the timer that ends the round when the emergency shuttle exits hyperspace.
+cmd-delayroundend-desc = Stops the timer that ends the round when the emergency shuttle exits hyperspace.
+cmd-delayroundend-help = Usage: delayroundend
 emergency-shuttle-command-round-yes = Round delayed.
 emergency-shuttle-command-round-no = Unable to delay round end.
 
 ## Dock emergency shuttle
-emergency-shuttle-command-dock-desc = Calls the emergency shuttle and docks it to the station... if it can.
+cmd-dockemergencyshuttle-desc = Calls the emergency shuttle and docks it to the station... if it can.
+cmd-dockemergencyshuttle-help = Usage: dockemergencyshuttle
 
 ## Launch emergency shuttle
-emergency-shuttle-command-launch-desc = Early launches the emergency shuttle if possible.
+cmd-launchemergencyshuttle-desc = Early launches the emergency shuttle if possible.
+cmd-launchemergencyshuttle-help = Usage: launchemergencyshuttle
 
 # Emergency shuttle
 emergency-shuttle-left = The Emergency Shuttle has left the station. Estimate {$transitTime} seconds until the shuttle arrives at CentComm.

makyo/substations

(cherry picked from commit 6a2afa562554d4e9c96db10ef3a32d8321d4cfba)

Resources/Locale/en-US/shuttles/emergency.ftl
@@ -1,15 +1,18 @@
 # L5: Emergency Shuttle→Evacuation Shuttle
 # Commands
 ## Delay shuttle round end
-emergency-shuttle-command-round-desc = Stops the timer that ends the round when the Evacuation Shuttle exits hyperspace.
+cmd-delayroundend-desc = Stops the timer that ends the round when the Evacuation Shuttle exits hyperspace.
+cmd-delayroundend-help = Usage: delayroundend
 emergency-shuttle-command-round-yes = Round delayed.
 emergency-shuttle-command-round-no = Unable to delay round end.
 
 ## Dock Evacuation Shuttle
-emergency-shuttle-command-dock-desc = Calls the Evacuation Shuttle and docks it to the station... if it can.
+cmd-dockemergencyshuttle-desc = Calls the Evacuation Shuttle and docks it to the station... if it can.
+cmd-dockemergencyshuttle-help = Usage: dockemergencyshuttle
 
 ## Launch Evacuation Shuttle
-emergency-shuttle-command-launch-desc = Early launches the Evacuation Shuttle if possible.
+cmd-launchemergencyshuttle-desc = Early launches the Evacuation Shuttle if possible.
+cmd-launchemergencyshuttle-help = Usage: launchemergencyshuttle
 
 # Evacuation Shuttle
 # L5