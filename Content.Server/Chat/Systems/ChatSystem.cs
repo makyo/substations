teecoding/Zona-14

fix: ghosts shouldn't see whisper obfuscation (#38202)

Content.Server/Chat/Systems/ChatSystem.cs
@@ -535,7 +535,7 @@ public sealed partial class ChatSystem : SharedChatSystem
             if (MessageRangeCheck(session, data, range) != MessageRangeCheckResult.Full)
                 continue; // Won't get logged to chat, and ghosts are too far away to see the pop-up, so we just won't send it to them.
 
-            if (data.Range <= WhisperClearRange)
+            if (data.Range <= WhisperClearRange || data.Observer)
                 _chatManager.ChatMessageToOne(ChatChannel.Whisper, message, wrappedMessage, source, false, session.Channel);
             //If listener is too far, they only hear fragments of the message
             else if (_examineSystem.InRangeUnOccluded(source, listener, WhisperMuffledRange))

makyo/substations

(cherry picked from commit bd853b60de27889e56033370eb82bdba5266d7db)

Content.Server/Chat/Systems/ChatSystem.cs
@@ -653,7 +653,7 @@ public sealed partial class ChatSystem : SharedChatSystem
             }
             // End L5 - hard of hearing trait
 
-            if (data.Range <= clearRange)
+            if (data.Range <= clearRange || data.Observer)
                 _chatManager.ChatMessageToOne(ChatChannel.Whisper, message, wrappedMessage, source, false, session.Channel);
             //If listener is too far, they only hear fragments of the message
             else if (_examineSystem.InRangeUnOccluded(source, listener, muffledRange))