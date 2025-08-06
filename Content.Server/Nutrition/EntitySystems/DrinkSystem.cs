4d8f1f4718f517eaa432e6da6cece60e68d3ae88
ScarKy0 <106310278+ScarKy0@users.noreply.github.com>
2025-08-06 15:00:32 +0200

    MessyDrinker for dogs (#38852)
    (cherry picked from commit 3d0a506f6dc596caf9583d7e5cb8b84fca17986c)

--- Content.Server/Nutrition/EntitySystems/DrinkSystem.cs
+++ Content.Server/Nutrition/EntitySystems/DrinkSystem.cs
@@ -246,14 +246,14 @@ public sealed class DrinkSystem : SharedDrinkSystem
         var beforeDrinkEvent = new BeforeIngestDrinkEvent(entity.Owner, drained, forceDrink);
         RaiseLocalEvent(args.Target.Value, ref beforeDrinkEvent);
 
-        if (drained.Volume == 0)
-            return;
+        _forensics.TransferDna(entity, args.Target.Value);
 
         _reaction.DoEntityReaction(args.Target.Value, solution, ReactionMethod.Ingestion);
 
-        _stomach.TryTransferSolution(firstStomach.Value.Owner, drained, firstStomach.Value.Comp1);
+        if (drained.Volume == 0)
+            return;
 
-        _forensics.TransferDna(entity, args.Target.Value);
+        _stomach.TryTransferSolution(firstStomach.Value.Owner, drained, firstStomach.Value.Comp1);
 
         if (!forceDrink && solution.Volume > 0)
             args.Repeat = true;
