88f81ac48109631da7d4ebf94d795848c57fa1c8
Perry Fraser <perry@frasers.org>
2025-08-11 20:57:10 -0400

    chore: mind filter migrations
    No shot this is all correct but it's okay because antagonists aren't
    real.

--- Content.Shared/Mind/Filters/JobMindFilter.cs
+++ Content.Shared/Mind/Filters/JobMindFilter.cs
@@ -10,12 +10,27 @@ namespace Content.Shared.Mind.Filters;
 /// </summary>
 public sealed partial class JobMindFilter : MindFilter
 {
-    [DataField(required: true)]
-    public ProtoId<JobPrototype> Job;
+    [DataField] // L5 - don't make required + make optional for below
+    public ProtoId<JobPrototype>? Job;
+
+    /// <summary>
+    /// L5 - porting of PickRandomPersonComponent.OnlyChoosableJobs to mind role
+    /// refactor. This is kinda hacky it should be a new filer probably but meh.
+    /// </summary>
+    [DataField]
+    public bool OnlyChoosableJobs;
 
     protected override bool ShouldRemove(Entity<MindComponent> mind, EntityUid? exclude, IEntityManager entMan, SharedMindSystem mindSys)
     {
         var jobSys = entMan.System<SharedJobSystem>();
-        return jobSys.MindHasJobWithId(mind, Job);
+
+        // Begin L5 additions - OnlyChoosableJobs support
+        if (OnlyChoosableJobs
+            && jobSys.MindTryGetJob(mind, out var jobProto)
+            && !jobProto.SetPreference)
+            return false;
+        // End L5 additions
+
+        return Job is not null && jobSys.MindHasJobWithId(mind, Job); // L5 null check
     }
 }
