14243d14abe972db8fb6552e9f64c0f468808817
No Elka <125199100+noelkathegod@users.noreply.github.com>
2025-08-13 04:34:50 -0700

    Censer tweaks part 2 (#4171)
    * Kill asphyx damage
    
    * UI goidacode
    
    * Locale
    
    * I think this is needed too???
    
    * Code stuff
    
    * Code stuff
    
    * Replace fuel with mindbreaker toxin
    
    * Move censer to lockers
    
    * Halve censer fuel capacity
    
    * I sure hope the branch won't fucking explode
    
    * Remove accidental line break
    
    * blyat
    
    * Update desc
    
    * Move to nested fill
    
    (cherry picked from commit a7df10b20da90a0f3370f06807477086fbb1b822)

--- Content.Server/_DV/CosmicCult/DeconversionSystem.cs
+++ Content.Server/_DV/CosmicCult/DeconversionSystem.cs
@@ -1,12 +1,10 @@
 using Content.Server._DV.CosmicCult.Components;
 using Content.Server.Bible.Components;
 using Content.Server.EUI;
-using Content.Server.Polymorph.Components;
-using Content.Server.Polymorph.Systems;
 using Content.Shared._DV.CosmicCult.Components.Examine;
 using Content.Shared._DV.CosmicCult.Components;
 using Content.Shared._DV.CosmicCult;
-using Content.Shared.Damage.Systems;
+using Content.Shared.Damage;
 using Content.Shared.DoAfter;
 using Content.Shared.IdentityManagement;
 using Content.Shared.Interaction;
@@ -40,7 +38,6 @@ public sealed class DeconversionSystem : EntitySystem
     [Dependency] private readonly SharedMindSystem _mind = default!;
     [Dependency] private readonly IPlayerManager _playerMan = default!;
     [Dependency] private readonly EuiManager _euiMan = default!;
-    [Dependency] private readonly PolymorphSystem _polymorph = default!;
 
     public override void Initialize()
     {
@@ -124,10 +121,7 @@ public sealed class DeconversionSystem : EntitySystem
             _popup.PopupEntity(Loc.GetString("cleanse-deconvert-attempt-notcorrupted", ("target", Identity.Entity(target.Value, EntityManager))), args.User, args.User);
             _popup.PopupCoordinates(Loc.GetString("cleanse-deconvert-attempt-rebound"), targetPosition, PopupType.MediumCaution);
             _damageable.TryChangeDamage(args.User, censer.FailedDeconversionDamage, true);
-
-            if (args.Target.HasValue)
-                _damageable.TryChangeDamage(args.Target.Value, censer.FailedDeconversionDamage, true);
-
+            _damageable.TryChangeDamage(args.Target, censer.FailedDeconversionDamage, true);
             _stun.TryKnockdown(target.Value, TimeSpan.FromSeconds(2), true);
             if (_mind.TryGetMind(target.Value, out _, out var mind) && _playerMan.TryGetSessionById(mind.UserId, out var session))
             {
@@ -140,12 +134,5 @@ public sealed class DeconversionSystem : EntitySystem
     private void DeconvertCultist(EntityUid uid)
     {
         RemComp<CosmicCultComponent>(uid);
-        if (TryComp<PolymorphedEntityComponent>(uid, out var polyComp)) // If the cultist is polymorphed, we revert the polymorph and deconvert the original entity too.
-        {
-            _polymorph.Revert((uid, polyComp));
-
-            if (polyComp.Parent.HasValue) // This surely won't cause any bugs with deconversion, right?
-                RemCompDeferred<CosmicCultComponent>(polyComp.Parent.Value);
-        }
     }
 }
