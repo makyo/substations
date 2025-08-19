c19629d619499490ed01717d5694983588794ae8
Madison Rye Progress <makyo@drab-makyo.com>
2025-08-18 19:57:50 -0700

    Toggleable, resprited

--- Content.Server/_L5/StaticField/StaticFieldSystem.cs
+++ Content.Server/_L5/StaticField/StaticFieldSystem.cs
@@ -1,30 +1,63 @@
 using Content.Server.Atmos.Components;
+using Content.Server.Atmos.EntitySystems;
+using Content.Server.Construction.Completions;
 using Content.Shared._L5.StaticField;
+using Content.Shared.DeviceLinking.Events;
+using Content.Shared.DeviceNetwork.Events;
 using Content.Shared.Power;
 using Content.Shared.Power.EntitySystems;
+using Robust.Server.Audio;
+using Robust.Shared.Audio;
 
 namespace Content.Server._L5.StaticField;
 
 public sealed class StaticFieldSystem : EntitySystem
 {
+    [Dependency] private readonly AirtightSystem _airtightSystem = default!;
+    [Dependency] private readonly AudioSystem _audioSystem = default!;
+    [Dependency] private readonly SharedPowerReceiverSystem _receiverSystem = default!;
 
     public override void Initialize()
     {
         base.Initialize();
         SubscribeLocalEvent<StaticFieldComponent, PowerChangedEvent>(OnPowerChanged);
+        SubscribeLocalEvent<StaticFieldComponent, SignalReceivedEvent>(OnSignalReceived);
     }
 
-    private void OnPowerChanged(Entity<StaticFieldComponent> ent, ref PowerChangedEvent evt)
+    private void OnPowerChanged(Entity<StaticFieldComponent> ent, ref PowerChangedEvent args)
     {
-        ent.Comp.Powered = evt.Powered;
-        if (evt.Powered)
+        SetState(ent, args.Powered);
+    }
+
+    private void OnSignalReceived(Entity<StaticFieldComponent> ent, ref SignalReceivedEvent args)
+    {
+        if (args.Port == ent.Comp.OffPort)
         {
-            EnsureComp<AirtightComponent>(ent);
+            _receiverSystem.SetPowerDisabled(ent, true);
+            SetState(ent, false);
         }
-        else
+        else if (args.Port == ent.Comp.OnPort)
         {
-            RemCompDeferred<AirtightComponent>(ent);
+            _receiverSystem.SetPowerDisabled(ent, false);
+            SetState(ent, true);
         }
+        else if (args.Port == ent.Comp.TogglePort)
+        {
+            _receiverSystem.TogglePower(ent, false);
+            SetState(ent, !ent.Comp.Powered);
+        }
+    }
+
+    private void SetState(Entity<StaticFieldComponent> ent, bool state)
+    {
+        ent.Comp.Powered = state;
+        EnsureComp<AirtightComponent>(ent, out var airtight);
+        _airtightSystem.SetAirblocked((ent, airtight), state);
+        _audioSystem.PlayLocal(
+            state ? ent.Comp.PowerUpSound : ent.Comp.PowerDownSound,
+            ent,
+            ent,
+            new AudioParams(1.0f, 1.0f, 3.0f, 3.0f, true, 0.0f));
         Dirty(ent);
     }
 }
--- Content.Shared/_L5/StaticField/StaticFieldComponent.cs
+++ Content.Shared/_L5/StaticField/StaticFieldComponent.cs
@@ -1,4 +1,9 @@
+using Content.Shared.DeviceLinking;
+using Content.Shared.Power;
+using Robust.Shared.Audio;
 using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
 
 namespace Content.Shared._L5.StaticField;
 
@@ -6,7 +11,20 @@ namespace Content.Shared._L5.StaticField;
 public sealed partial class StaticFieldComponent : Component
 {
     [DataField, AutoNetworkedField]
+    public PowerDeviceVisuals Status = PowerDeviceVisuals.Powered;
+
+    [DataField, AutoNetworkedField]
     public bool Powered = false;
 
-    public bool AlwaysPowered = false;
+    [DataField]
+    public ProtoId<SinkPortPrototype> OnPort = "On";
+
+    [DataField]
+    public ProtoId<SinkPortPrototype> OffPort = "Off";
+
+    [DataField]
+    public ProtoId<SinkPortPrototype> TogglePort = "Toggle";
+
+    public SoundSpecifier PowerUpSound = new SoundPathSpecifier("/Audio/_L5/StaticField/powerup.ogg");
+    public SoundSpecifier PowerDownSound = new SoundPathSpecifier("/Audio/_L5/StaticField/powerdown.ogg");
 }
