2532df381a9f883511ecd06d1d45059cc18c1565
No Elka <125199100+noelkathegod@users.noreply.github.com>
2025-08-27 10:20:41 -0700

    Minor-ish cult ability rebalance (#4260)
    Change stuff
    
    (cherry picked from commit a8a2361cccf83ae85d5a6c43804fb7647b4ecdf9)

--- Content.Server/_DV/CosmicCult/MonumentSystem.cs
+++ Content.Server/_DV/CosmicCult/MonumentSystem.cs
@@ -17,6 +17,7 @@ using Content.Shared.Damage.Systems;
 using Content.Shared.Interaction;
 using Content.Shared.Popups;
 using Content.Shared.Stacks;
+using Content.Shared.Temperature.Components;
 using Content.Shared.UserInterface;
 using Robust.Server.GameObjects;
 using Robust.Shared.Audio.Systems;
