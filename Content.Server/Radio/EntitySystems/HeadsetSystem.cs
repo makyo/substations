d775217f20a15a2ee1e305b7031d6fa89f1de4de
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2025-10-04 04:24:42 -0700

    Predict EMPs (#39802)
    * predicted emps
    
    * fixes
    
    * fix
    
    * review
    
    (cherry picked from commit 5227489360126521b5ded181dc63ee29f73c6d87)

--- Content.Server/Radio/EntitySystems/HeadsetSystem.cs
+++ Content.Server/Radio/EntitySystems/HeadsetSystem.cs
@@ -1,5 +1,4 @@
 using Content.Server.Chat.Systems;
-using Content.Server.Emp;
 using Content.Shared.Radio.Components;
 using Content.Shared.Cuffs; // DeltaV
 using Content.Shared.Cuffs.Components; // DeltaV
@@ -28,8 +27,6 @@ public sealed class HeadsetSystem : SharedHeadsetSystem
         SubscribeLocalEvent<HeadsetComponent, EncryptionChannelsChangedEvent>(OnKeysChanged);
 
         SubscribeLocalEvent<WearingHeadsetComponent, EntitySpokeEvent>(OnSpeak);
-
-        SubscribeLocalEvent<HeadsetComponent, EmpPulseEvent>(OnEmpPulse);
     }
 
     private void OnKeysChanged(EntityUid uid, HeadsetComponent component, EncryptionChannelsChangedEvent args)
@@ -86,7 +83,6 @@ public sealed class HeadsetSystem : SharedHeadsetSystem
     protected override void OnGotUnequipped(EntityUid uid, HeadsetComponent component, GotUnequippedEvent args)
     {
         base.OnGotUnequipped(uid, component, args);
-        component.IsEquipped = false;
         RemComp<ActiveRadioComponent>(uid);
         RemComp<WearingHeadsetComponent>(args.Equipee);
     }
@@ -98,6 +94,9 @@ public sealed class HeadsetSystem : SharedHeadsetSystem
 
         if (component.Enabled == value)
             return;
+
+        component.Enabled = value;
+        Dirty(uid, component);
 
         if (!value)
         {
@@ -129,14 +128,5 @@ public sealed class HeadsetSystem : SharedHeadsetSystem
 
         if (TryComp(parent, out ActorComponent? actor))
             _netMan.ServerSendMessage(args.ChatMsg, actor.PlayerSession.Channel);
-    }
-
-    private void OnEmpPulse(EntityUid uid, HeadsetComponent component, ref EmpPulseEvent args)
-    {
-        if (component.Enabled)
-        {
-            args.Affected = true;
-            args.Disabled = true;
-        }
     }
 }
