cffb3ac8f8d054072b06322d0877c601cb88f000
SlamBamActionman <83650252+slambamactionman@users.noreply.github.com>
2025-12-03 03:55:29 -0800

    Add status effect support to Traits, change PainNumbness to be a status effect (#41646)
    * Initial commit
    
    * Review comments
    
    * Jobify
    
    * Prototype(effect)
    
    (cherry picked from commit d0a784b9e63c75c1c3e7c9207cc4a59657ca94ef)

--- Content.Shared/Traits/TraitPrototype.cs
+++ Content.Shared/Traits/TraitPrototype.cs
@@ -1,4 +1,5 @@
 using Content.Shared.Humanoid.Prototypes;
+using Content.Shared.Roles;
 using Content.Shared.Whitelist;
 using Robust.Shared.Prototypes;
 
@@ -40,9 +41,17 @@ public sealed partial class TraitPrototype : IPrototype
 
     /// <summary>
     /// The components that get added to the player, when they pick this trait.
+    /// NOTE: When implementing a new trait, it's preferable to add it as a status effect instead if possible.
     /// </summary>
     [DataField]
-    public ComponentRegistry Components { get; private set; } = default!;
+    [Obsolete("Use JobSpecial instead.")]
+    public ComponentRegistry Components { get; private set; } = new();
+
+    /// <summary>
+    /// Special effects applied to the player who takes this Trait.
+    /// </summary>
+    [DataField(serverOnly: true)]
+    public List<JobSpecial> Specials { get; private set; } = new();
 
     /// <summary>
     /// DeltaV - Components that get added to the player, overriding any existing instances of the component if they exist.
