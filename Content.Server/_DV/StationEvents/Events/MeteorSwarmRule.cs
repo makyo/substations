1384005659b7a0126df9539adbb9c92cd5eae95c
Charlie Morley <cmorley191@gmail.com>
2025-12-08 06:41:56 -0800

    Protect arrivals and evac from meteors (slightly) (#4820)
    * Protect arrivals and evac from meteors (slightly)
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * change meteor protection zone to a MarkerBase
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    
    (cherry picked from commit 14f6ebbd146a1dcf04d1a2c497fe92a31d9c4c51)

--- Content.Server/_DV/StationEvents/Events/MeteorSwarmRule.cs
+++ Content.Server/_DV/StationEvents/Events/MeteorSwarmRule.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Numerics;
 using Content.Server.Atmos.Components;
 using Content.Server.GameTicking.Rules.Components;
@@ -7,6 +8,7 @@ using Robust.Shared.Map;
 using Robust.Shared.Map.Components;
 using Robust.Shared.Physics.Components;
 using Robust.Shared.Physics.Systems;
+using Robust.Shared.Random;
 using Robust.Shared.Spawners;
 
 namespace Content.Server.StationEvents.Events
@@ -68,24 +70,107 @@ namespace Content.Server.StationEvents.Events
 
             var center = playableArea.Value.Center;
 
+            var protectedAreas = new List<(MapCoordinates center, float radiusSquared, float protectionRate)>();
+
+            var protectedAreaQuery = AllEntityQuery<AntiMeteorZoneComponent, TransformComponent>();
+            while (protectedAreaQuery.MoveNext(out var protectedEntityUid, out var antiZone, out var transform))
+            {
+                protectedAreas.Add((
+                    center: _transform.ToMapCoordinates(transform.Coordinates),
+                    radiusSquared: MathF.Pow(antiZone.ZoneRadius, 2f),
+                    protectionRate: antiZone.AvoidanceRate
+                ));
+            }
+            // dang, I wish I could use Linq on queries
+            var standardRateProtectedAreaQuery = AllEntityQuery<AntiMeteorZoneStandardRateComponent, TransformComponent>();
+            while (standardRateProtectedAreaQuery.MoveNext(out var protectedEntityUid, out var antiZone, out var transform))
+            {
+                protectedAreas.Add((
+                    center: _transform.ToMapCoordinates(transform.Coordinates),
+                    radiusSquared: MathF.Pow(antiZone.ZoneRadius, 2f),
+                    protectionRate: antiZone.AvoidanceRate
+                ));
+            }
+
+            float maxImpactTime = 0;
             for (var i = 0; i < component.MeteorsPerWave; i++)
             {
-                var angle = new Angle(RobustRandom.NextFloat() * MathF.Tau);
-                var offset = angle.RotateVec(new Vector2((maximumDistance - minimumDistance) * RobustRandom.NextFloat() + minimumDistance, 0));
-                var spawnPosition = new MapCoordinates(center + offset, mapId);
+                var protectedAreasThisMeteor =
+                    protectedAreas.Where(protectedArea => RobustRandom.Prob(protectedArea.protectionRate)).ToList();
+
+                MapCoordinates spawnPosition;
+                Vector2 velocity;
+
+                int targetingAttempts = 0;
+                bool targetingSafe;
+                do
+                {
+                    var angle = new Angle(RobustRandom.NextFloat() * MathF.Tau);
+                    var offset = angle.RotateVec(new Vector2((maximumDistance - minimumDistance) * RobustRandom.NextFloat() + minimumDistance, 0));
+                    spawnPosition = new MapCoordinates(
+                        target.X + targetSpread * (2f * RobustRandom.NextFloat() - 1f) + offset.X,
+                        target.Y + targetSpread * (2f * RobustRandom.NextFloat() - 1f) + offset.Y,
+                        mapId
+                    );
+                    velocity = -offset.Normalized() * component.MeteorVelocity;
+
+                    targetingSafe = true;
+                    foreach (var protectedArea in protectedAreasThisMeteor)
+                    {
+                        var timeUntilClosestApproachToProtectedArea = TimeUntilClosestApproach(spawnPosition, velocity, protectedArea.center);
+                        var pointOfClosestApproachToProtectedArea = spawnPosition.Position + timeUntilClosestApproachToProtectedArea * velocity;
+                        if (
+                            Vector2.DistanceSquared(protectedArea.center.Position, pointOfClosestApproachToProtectedArea)
+                            < protectedArea.radiusSquared
+                        )
+                        {
+                            targetingSafe = false;
+                            break;
+                        }
+                    }
+
+                    targetingAttempts++;
+                }
+                while (!targetingSafe && targetingAttempts <= 3); // attempt to avoid the protected areas a few times
+
                 var meteor = Spawn(proto, spawnPosition);
                 var physics = EntityManager.GetComponent<PhysicsComponent>(meteor);
                 _physics.SetBodyStatus(meteor, physics, BodyStatus.InAir);
                 _physics.SetLinearDamping(meteor, physics, 0f);
                 _physics.SetAngularDamping(meteor, physics, 0f);
-                _physics.ApplyLinearImpulse(meteor, -offset.Normalized() * component.MeteorVelocity * physics.Mass, body: physics);
-                _physics.ApplyAngularImpulse(
-                    meteor,
-                    physics.Mass * ((component.MaxAngularVelocity - component.MinAngularVelocity) * RobustRandom.NextFloat() + component.MinAngularVelocity),
-                    body: physics);
+                _physics.ApplyLinearImpulse(meteor, velocity * physics.Mass, body: physics);
+                _physics.SetAngularVelocity(meteor, (component.MaxAngularVelocity - component.MinAngularVelocity) * RobustRandom.NextFloat() + component.MinAngularVelocity, body: physics);
 
-                EnsureComp<TimedDespawnComponent>(meteor).Lifetime = 120f;
+                EnsureComp<TimedDespawnComponent>(meteor).Lifetime = component.MeteorLifetime;
+
+                var timeUntilClosestApproachToTarget = TimeUntilClosestApproach(spawnPosition, velocity, target);
+                if (i == 0 || timeUntilClosestApproachToTarget > maxImpactTime)
+                {
+                    maxImpactTime = timeUntilClosestApproachToTarget;
+                }
             }
+
+            component.WaveCounter--;
+            if (component.WaveCounter <= 0)
+            {
+                // DeltaV space maps are quite large so it can take 1-2 minutes for the meteors to arrive.
+                // Delay "meteor swarm finished" announcement until just after last meteor is scheduled to strike
+                component.IsEnding = true;
+                component.Cooldown += maxImpactTime + (5f * RobustRandom.NextFloat()) + 5f;
+            }
+            else
+            {
+                component.Cooldown += (component.MaximumCooldown - component.MinimumCooldown) * RobustRandom.NextFloat() + component.MinimumCooldown;
+            }
+        }
+
+        private static float TimeUntilClosestApproach(MapCoordinates startPoint, Vector2 velocity, MapCoordinates approachPoint)
+        {
+            var approachPointRelativeToStartPoint = new Vector2(approachPoint.X - startPoint.X, approachPoint.Y - startPoint.Y);
+            return (
+                (approachPointRelativeToStartPoint.X * velocity.X + approachPointRelativeToStartPoint.Y * velocity.Y) /
+                (MathF.Pow(velocity.X, 2f) + MathF.Pow(velocity.Y, 2f))
+            );
         }
     }
 }
