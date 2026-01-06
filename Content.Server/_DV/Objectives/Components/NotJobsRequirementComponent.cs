47fa5faa849734ef066ae64672c250c648362b7c
Madison Rye Progress <makyo@drab-makyo.com>
2026-01-06 12:22:20 -0800

    Fix for mind role prediction

--- Content.Server/_DV/Objectives/Components/NotJobsRequirementComponent.cs
+++ Content.Server/_DV/Objectives/Components/NotJobsRequirementComponent.cs
@@ -1,4 +1,4 @@
-using Content.Server.Objectives.Systems;
+using Content.Server._DV.Objectives.Systems;
 using Content.Shared.Roles;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.List;
 
