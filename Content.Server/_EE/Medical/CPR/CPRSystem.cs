d7e54f0f3ae4c2445453d69fb03ce3c7a09a4525
Madison Rye Progress <makyo@drab-makyo.com>
2026-01-09 12:13:22 -0800

    ovewrote

--- Content.Server/_EE/Medical/CPR/CPRSystem.cs
+++ Content.Server/_EE/Medical/CPR/CPRSystem.cs
@@ -1,157 +1,95 @@
-using Content.Server.Atmos.Rotting;
-using Content.Server.DoAfter;
-using Content.Server.Popups;
-using Content.Shared._EE.CCVars;
-using Content.Shared.Atmos.Rotting;
-using Content.Shared.Damage;
+using Content.Shared._DV.Body.Components;
+using Content.Shared._DV.Body.Events;
 using Content.Shared.DoAfter;
-using Content.Shared.Inventory;
-using Content.Shared.Mobs;
 using Content.Shared.Mobs.Components;
 using Content.Shared.Mobs.Systems;
-using Content.Shared.Nutrition.EntitySystems;
+using Content.Shared.Popups;
 using Content.Shared.Verbs;
-using Robust.Server.Audio;
-using Robust.Shared.Audio;
-using Robust.Shared.Configuration;
-using Robust.Shared.Utility;
-// ReSharper disable InconsistentNaming
 
-namespace Content.Server._EE.Medical.CPR;
+namespace Content.Shared._DV.Body.Systems;
 
 public sealed class CPRSystem : EntitySystem
 {
-    [Dependency] private readonly PopupSystem _popupSystem = default!;
-    [Dependency] private readonly DoAfterSystem _doAfterSystem = default!;
     [Dependency] private readonly MobStateSystem _mobStateSystem = default!;
-    [Dependency] private readonly IngestionSystem _ingestion = default!; // L5 - Food to IngestionSystem
-    [Dependency] private readonly DamageableSystem _damageable = default!;
-    [Dependency] private readonly RottingSystem _rottingSystem = default!;
-    [Dependency] private readonly InventorySystem _inventory = default!;
-    [Dependency] private readonly AudioSystem _audio = default!;
-    [Dependency] private readonly IConfigurationManager _configuration = default!;
+    [Dependency] private readonly SharedDoAfterSystem _doAfterSystem = default!;
+    [Dependency] private readonly SharedPopupSystem _popupSystem = default!;
 
     public override void Initialize()
     {
         base.Initialize();
-        SubscribeLocalEvent<CPRTrainingComponent, GetVerbsEvent<InnateVerb>>(AddCPRVerb);
-        SubscribeLocalEvent<CPRTrainingComponent, Shared._EE.Medical.CPRDoAfterEvent>(OnCPRDoAfter);
+
+        SubscribeLocalEvent<CanDoCPRComponent, CPRFinishedEvent>(OnCprFinished);
+        SubscribeLocalEvent<MobStateComponent, GetVerbsEvent<AlternativeVerb>>(AddCPRVerb);
     }
 
-    private void AddCPRVerb(Entity<CPRTrainingComponent> performer, ref GetVerbsEvent<InnateVerb> args)
+    private void OnCprFinished(Entity<CanDoCPRComponent> entity, ref CPRFinishedEvent ev)
     {
-        // Begin L5 additions
-        // Respect CVar
-        if (!_configuration.GetCVar(CPRCCVars.EnableCPR))
+        if (!ev.Target.HasValue || ev.Handled)
             return;
 
-        // Track doafter being active
-        if (performer.Comp.DoAfter is not null)
-            return;
-        // End L5 additions
-
-
-        if (!args.CanInteract || !args.CanAccess || !TryComp<MobStateComponent>(args.Target, out var targetState)
-            || targetState.CurrentState == MobState.Alive)
-            return;
-
-        var target = args.Target;
-        InnateVerb verb = new()
+        if (!ev.Cancelled && !_mobStateSystem.IsAlive(ev.Target.Value)) // Allow CPR on Criticial and Dead patients.
         {
-            Act = () => { StartCPR(performer, target); },
-            Text = Loc.GetString("cpr-verb"),
-            Icon = new SpriteSpecifier.Rsi(new("Interface/Alerts/human_alive.rsi"), "health4"),
-            Priority = 2
-        };
+            var comp = EnsureComp<AffectedByCPRComponent>(ev.Target.Value); // Enables the Crit Patient to breathe.
+            comp.IsActive = true;
+            ev.Repeat = true;
 
-        args.Verbs.Add(verb);
+            var msgUser = Loc.GetString("cpr-popup-continue-user", ("patient", ev.Target.Value));
+            var msgOthers = Loc.GetString("cpr-popup-continue-others", ("patient", ev.Target.Value), ("provider", entity.Owner));
+            _popupSystem.PopupPredicted(msgUser, msgOthers, entity.Owner, entity.Owner);
+        }
+        else
+        {
+            RemComp<AffectedByCPRComponent>(ev.Target.Value); // Removes breathing while crit.
+
+            var msgUser = Loc.GetString("cpr-popup-stop-user", ("patient", ev.Target.Value));
+            var msgOthers = Loc.GetString("cpr-popup-stop-others", ("patient", ev.Target.Value), ("provider", entity.Owner));
+            _popupSystem.PopupPredicted(msgUser, msgOthers, entity.Owner, entity.Owner);
+        }
+        ev.Handled = true;
     }
 
-    private void StartCPR(Entity<CPRTrainingComponent> performer, EntityUid target)
+    private void StartCPR(EntityUid user, EntityUid target, float cprTime)
     {
-        // Begin L5 additions - track doafter being active (should be unreachable)
-        if (performer.Comp.DoAfter is not null)
+        if (HasComp<AffectedByCPRComponent>(target))
             return;
-        // End L5 additions
 
-        if (HasComp<RottingComponent>(target))
+        var doAfterArgs = new DoAfterArgs(EntityManager, user, cprTime, new CPRFinishedEvent(), user, target: target)
         {
-            _popupSystem.PopupEntity(Loc.GetString("cpr-target-rotting", ("entity", target)), performer, performer);
-            return;
-        }
-
-        if (_inventory.TryGetSlotEntity(target, "outerClothing", out var outer))
-        {
-            _popupSystem.PopupEntity(Loc.GetString("cpr-must-remove", ("clothing", outer)), performer, performer);
-            return;
-        }
-
-        // L5 - adjust for food refactor:
-        if (!_ingestion.HasMouthAvailable(performer, performer) || _ingestion.HasMouthAvailable(target, performer))
-            return;
-
-        _popupSystem.PopupEntity(Loc.GetString("cpr-start-second-person", ("target", target)), target, performer);
-        _popupSystem.PopupEntity(Loc.GetString("cpr-start-second-person-patient", ("user", performer)), target, target);
-
-        var doAfterArgs = new DoAfterArgs(
-            EntityManager, performer, performer.Comp.DoAfterDuration, new Shared._EE.Medical.CPRDoAfterEvent(), performer, target,
-            performer)
-        {
+            BreakOnDamage = true,
             BreakOnMove = true,
+            BreakOnWeightlessMove = false,
+            BreakOnHandChange = false,
             NeedHand = true,
-            BlockDuplicate = true
         };
 
-        // Begin L5 modifications - track doafter
-        _doAfterSystem.TryStartDoAfter(doAfterArgs, out var id);
-        performer.Comp.DoAfter = id;
-        // End L5 modifications
-
-        var playingStream = _audio.PlayPvs(performer.Comp.CPRSound, performer, AudioParams.Default.WithLoop(true));
-        if (!playingStream.HasValue)
-            return;
-
-        performer.Comp.CPRPlayingStream = playingStream.Value.Entity;
+        AddComp<AffectedByCPRComponent>(target);
+        var msgUser = Loc.GetString("cpr-popup-start-user", ("patient", target));
+        var msgOthers = Loc.GetString("cpr-popup-start-others", ("patient", target), ("provider", user));
+        _popupSystem.PopupPredicted(msgUser, msgOthers, user, user, PopupType.Medium);
+        _doAfterSystem.TryStartDoAfter(doAfterArgs);
     }
 
-    private void OnCPRDoAfter(Entity<CPRTrainingComponent> performer, ref Shared._EE.Medical.CPRDoAfterEvent args)
+    private void AddCPRVerb(Entity<MobStateComponent> entity, ref GetVerbsEvent<AlternativeVerb> ev)
     {
-        if (args.Cancelled || args.Handled || !args.Target.HasValue)
-        {
-            performer.Comp.CPRPlayingStream = _audio.Stop(performer.Comp.CPRPlayingStream);
-            performer.Comp.DoAfter = null; // L5 - track doafter
+        if (entity.Owner == ev.User
+            || !ev.CanInteract
+            || _mobStateSystem.IsAlive(entity.Owner)
+            || !TryComp<CanDoCPRComponent>(ev.User, out var cprComp))
             return;
-        }
 
-        // L5 changes:
-        // - Respect CVars
-        // - No resuscitation chance
-        _damageable.TryChangeDamage(args.Target,
-            new DamageSpecifier
-            {
-                DamageDict =
-                {
-                    ["Asphyxiation"] = performer.Comp.DoAfterDuration.Seconds * _configuration.GetCVar(CPRCCVars.CPRAirlossReductionMultiplier),
-                },
-            },
-            true,
-            origin: performer);
+        var alreadyAffected = HasComp<AffectedByCPRComponent>(ev.Target);
 
-
-        _rottingSystem.ReduceAccumulator(
-            (EntityUid)args.Target,
-            performer.Comp.DoAfterDuration * _configuration.GetCVar(CPRCCVars.CPRRotReductionMultiplier));
-
-
-        var isAlive = _mobStateSystem.IsAlive(args.Target.Value);
-        args.Repeat = !isAlive;
-
-        // Begin L5 modifications - track doafter
-        if (isAlive)
+        var user = ev.User;
+        var target = ev.Target;
+        AlternativeVerb verb = new()
         {
-            performer.Comp.CPRPlayingStream = _audio.Stop(performer.Comp.CPRPlayingStream);
-            performer.Comp.DoAfter = null;
-        }
-        // End L5 modifications
+            Act = () => StartCPR(user, target, cprComp.TimeLength),
+            Text = Loc.GetString("cpr-verb-start"),
+            Priority = 2,
+            Disabled = alreadyAffected,
+            Message = alreadyAffected ? Loc.GetString("cpr-verb-disabled-description") : Loc.GetString("cpr-verb-description"),
+        };
+
+        ev.Verbs.Add(verb);
     }
 }
