ea7024a18e06200df5e214610cfffa7c0896985c
Madison Rye Progress <makyo@drab-makyo.com>
2026-01-09 12:28:06 -0800

    Some ovewriting

--- Content.Server/_L5/Traits/Synthetic/SynthSystem.cs
+++ Content.Server/_L5/Traits/Synthetic/SynthSystem.cs
@@ -19,6 +19,7 @@ using Content.Shared.Popups;
 using Content.Shared.Speech.Muting;
 using Content.Shared.StatusEffect;
 using Content.Shared._L5.Traits.Synthetic;
+using Content.Shared.Body.Components;
 using Content.Shared.Emp;
 using Robust.Shared.Prototypes;
 
