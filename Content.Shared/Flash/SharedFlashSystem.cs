3757c08bef8deac2dadeea349cb56a2a8d902f0d
Madison Rye Progress <makyo@drab-makyo.com>
2026-01-09 14:39:58 -0800

    Coscult merge fixes

--- Content.Shared/Flash/SharedFlashSystem.cs
+++ Content.Shared/Flash/SharedFlashSystem.cs
@@ -154,9 +154,10 @@ public abstract class SharedFlashSystem : EntitySystem
         float slowTo,
         bool displayPopup = true,
         bool melee = false,
-        TimeSpan? stunDuration = null)
+        TimeSpan? stunDuration = null,
+        bool ignoreProtection = false) //DeltaV: allow flashing to ignore flash protection
     {
-        var attempt = new FlashAttemptEvent(target, user, used);
+        var attempt = new FlashAttemptEvent(target, user, used, ignoreProtection); //DeltaV: allow flashing to ignore flash protection
         RaiseLocalEvent(target, ref attempt, true);
 
         if (attempt.Cancelled)
--- Content.Shared/_DV/CosmicCult/Components/CosmicPurifiableComponent.cs
+++ Content.Shared/_DV/CosmicCult/Components/CosmicPurifiableComponent.cs
@@ -1,14 +1,27 @@
+using Robust.Shared.Audio;
 using Robust.Shared.Prototypes;
-using Robust.Shared.Serialization;
 
 namespace Content.Shared._DV.CosmicCult.Components;
 
-/// <summary>
-/// Component for Cosmic Cult's own radio jammer, because I'm not refactoring RadioJammerSystem.
-/// </summary>
 [RegisterComponent]
-public sealed partial class CosmicJammerComponent : Component
+[AutoGenerateComponentPause]
+public sealed partial class CosmicPurifiableComponent : Component
 {
-    [DataField] public float Range = 18f;
-    [DataField] public bool Active = false;
+    [DataField]
+    public EntProtoId PurgeVFX = "CleanseEffectVFX";
+
+    [DataField]
+    public SoundSpecifier PurgeSound = new SoundPathSpecifier("/Audio/_DV/CosmicCult/cleanse_deconversion.ogg");
+
+    [DataField]
+    public TimeSpan CleanseTime = TimeSpan.FromSeconds(35);
+
+    [DataField]
+    public TimeSpan CleanseTimeChaplain = TimeSpan.FromSeconds(20);
+
+    ///<summary>
+    /// If true, only an entity with a BibleUserComponent can purge this entity
+    ///</summary>
+    [DataField]
+    public bool BibleUserRequired;
 }
