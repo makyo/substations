34e38c79ce92cdeb07112e90f28e16060b3894cb
pathetic meowmeow <uhhadd@gmail.com>
2026-01-13 21:57:08 -0800

    Decouple gibbing from the body system (#42405)
    * Decouple gibbing from the body system
    
    * allow gibs that don't drop giblets
    
    * pass through user
    
    * prediction gon
    
    * comment
    
    * destructible
    
    * playpvs
    
    * very very very very very very very minor cleanup
    
    ---------
    
    Co-authored-by: Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
    
    (cherry picked from commit fb133494cc3bedf82e9b60a434574e9029e04244)

--- Content.Shared/Kitchen/SharedKitchenSpikeSystem.cs
+++ Content.Shared/Kitchen/SharedKitchenSpikeSystem.cs
@@ -1,12 +1,11 @@
 using Content.Shared.Administration.Logs;
-using Content.Shared.Body.Part; // DeltaV
-using Content.Shared.Body.Systems;
 using Content.Shared.Damage.Systems;
 using Content.Shared.Database;
 using Content.Shared.Destructible;
 using Content.Shared.DoAfter;
 using Content.Shared.DragDrop;
 using Content.Shared.Examine;
+using Content.Shared.Gibbing;
 using Content.Shared.Hands;
 using Content.Shared.Humanoid;
 using Content.Shared.IdentityManagement;
@@ -42,7 +41,7 @@ public sealed class SharedKitchenSpikeSystem : EntitySystem
     [Dependency] private readonly MobStateSystem _mobStateSystem = default!;
     [Dependency] private readonly SharedAppearanceSystem _appearanceSystem = default!;
     [Dependency] private readonly SharedAudioSystem _audioSystem = default!;
-    [Dependency] private readonly SharedBodySystem _bodySystem = default!;
+    [Dependency] private readonly GibbingSystem _gibbing = default!;
     [Dependency] private readonly SharedContainerSystem _containerSystem = default!;
     [Dependency] private readonly SharedDoAfterSystem _doAfterSystem = default!;
     [Dependency] private readonly SharedInteractionSystem _interaction = default!;
@@ -319,15 +318,7 @@ public sealed class SharedKitchenSpikeSystem : EntitySystem
         // Gib the victim if there is nothing else to butcher.
         if (butcherable.SpawnedEntities.Count == 0)
         {
-            // DeltaV - Gib the body, then body parts, but leave the organs
-            var gibs = _bodySystem.GibBody(args.Target.Value, true);
-
-            foreach (var gib in gibs)
-            {
-                if (HasComp<BodyPartComponent>(gib))
-                    PredictedQueueDel(gib);
-            }
-            // END DeltaV
+            _gibbing.Gib(args.Target.Value);
 
             var logSeverity = HasComp<HumanoidAppearanceComponent>(args.Target) ? LogImpact.Extreme : LogImpact.High;
 
