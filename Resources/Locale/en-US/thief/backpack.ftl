deleteddata271-ship-it/project-fire-32

update communicator kit description for voice mask implanter (#42115)
update thief backpack description for voice mask implanter

Resources/Locale/en-US/thief/backpack.ftl
@@ -54,7 +54,7 @@ thief-backpack-category-communicator-description =
     Money is power, and secrets are money. Use your silver tongue
     and wealth to subvert the station.
     Includes: Master key for all station channels, a CyberSun pen,
-    voice chameleon mask, and 20k spesos inside a briefcase.
+    voice mask implanter, and 20k spesos inside a briefcase.
 
 thief-backpack-category-smuggler-name = Smuggler Kit
 thief-backpack-category-smuggler-description =

makyo/substations

(cherry picked from commit cf2596118626491a799b16808a852b9b71ed558f)

Resources/Locale/en-US/thief/backpack.ftl
@@ -57,7 +57,7 @@ thief-backpack-category-communicator-description =
     Money is power, and secrets are money. Use your silver tongue
     and wealth to subvert the station.
     Includes: Master key for all station channels, a CNZ pen,
-    voice chameleon mask, and 20k RACs inside a briefcase.
+    voice mask implanter, and 20k RACs inside a briefcase.
 
 thief-backpack-category-smuggler-name = Smuggler Kit
 thief-backpack-category-smuggler-description =