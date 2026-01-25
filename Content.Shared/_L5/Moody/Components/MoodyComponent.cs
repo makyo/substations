bec380661c36400e801399093f7ddae7f3c298ec
Madison Rye Progress <makyo@drab-makyo.com>
2026-01-25 14:51:41 -0800

    Depression trait gives you moody.

--- Content.Shared/_L5/Moody/Components/MoodyComponent.cs
+++ Content.Shared/_L5/Moody/Components/MoodyComponent.cs
@@ -25,11 +25,11 @@ public sealed partial class MoodyComponent : Component
     /// Minimum time between pain popups.
     /// </summary>
     [DataField]
-    public TimeSpan MinimumPopupDelay = TimeSpan.FromSeconds(5);
+    public TimeSpan MinimumPopupDelay = TimeSpan.FromSeconds(30);
 
     /// <summary>
     /// Maximum time between pain popups.
     /// </summary>
     [DataField]
-    public TimeSpan MaximumPopupDelay = TimeSpan.FromSeconds(40);
+    public TimeSpan MaximumPopupDelay = TimeSpan.FromSeconds(60);
 };
