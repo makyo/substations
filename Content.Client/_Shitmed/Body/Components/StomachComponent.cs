b1f5afb6872757e672a45332fb3bc9bfb38705ed
Madison Rye Progress <makyo@drab-makyo.com>
2026-01-30 10:41:54 -0800

    Remove shitmed

--- Content.Client/_Shitmed/Body/Components/StomachComponent.cs
+++
@@ -1,5 +0,0 @@
-
-// L5 - I... I think? I THINK this is unneeded now?
-// namespace Content.Client._Shitmed.Body.Components;
-// [RegisterComponent]
-// public sealed partial class ShitmedStomachComponent : Component { }
