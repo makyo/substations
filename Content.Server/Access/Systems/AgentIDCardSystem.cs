479c3a6fb0934c903fefbbbe2fe92e6de6052693
Leah <leahcatdev@gmail.com>
2026-02-13 13:42:16 -0800

    Identity Mask now updates equipped Agent ID name (#42772)
    * add identity mask updating agent id functionality
    
    * fix indentation
    
    * add check for voice mask item
    
    * use ChangeIDName field instead
    
    * Minor cleanup
    
    * remove trycomp
    
    ---------
    
    Co-authored-by: beck-thompson <beck314159@hotmail.com>
    
    (cherry picked from commit 0e86bd45bd422e910b0ed134488ba0fa85782418)

--- Content.Server/Access/Systems/AgentIDCardSystem.cs
+++ Content.Server/Access/Systems/AgentIDCardSystem.cs
@@ -13,10 +13,12 @@ using Content.Shared._DV.Access.Components; // DeltaV
 using Content.Shared._DV.NanoChat; // DeltaV
 using Content.Server.Clothing.Systems;
 using Content.Server.Implants;
+using Content.Server.VoiceMask;
 using Content.Shared.Implants;
 using Content.Shared.Inventory;
 using Content.Shared.Lock;
 using Content.Shared.PDA;
+using Content.Shared.VoiceMask;
 using Content.Shared._L5.Contract; // L5
 
 namespace Content.Server.Access.Systems
@@ -42,6 +44,7 @@ namespace Content.Server.Access.Systems
             SubscribeLocalEvent<AgentIDCardComponent, AgentIDCardJobChangedMessage>(OnJobChanged);
             SubscribeLocalEvent<AgentIDCardComponent, AgentIDCardJobIconChangedMessage>(OnJobIconChanged);
             SubscribeLocalEvent<AgentIDCardComponent, InventoryRelayedEvent<ChameleonControllerOutfitSelectedEvent>>(OnChameleonControllerOutfitChangedItem);
+            SubscribeLocalEvent<AgentIDCardComponent, InventoryRelayedEvent<VoiceMaskNameUpdatedEvent>>(OnVoiceMaskNameChanged);
             SubscribeLocalEvent<AgentIDCardComponent, AgentIDCardNumberChangedMessage>(OnNumberChanged); // DeltaV
             SubscribeLocalEvent<AgentIDCardComponent, AgentIdCardContractChangedMessage>(OnContractChanged); // L5
         }
@@ -83,6 +86,17 @@ namespace Content.Server.Access.Systems
                 return;
 
             _chameleon.SetSelectedPrototype(ent, comp.IdCard);
+        }
+
+        private void OnVoiceMaskNameChanged(Entity<AgentIDCardComponent> ent, ref InventoryRelayedEvent<VoiceMaskNameUpdatedEvent> args)
+        {
+            if (!TryComp<IdCardComponent>(ent, out var idCard))
+                return;
+
+            if (!args.Args.VoiceMask.Comp.ChangeIDName)
+                return;
+
+            _cardSystem.TryChangeFullName(ent, args.Args.NewName, idCard);
         }
 
         // DeltaV - Add number change handler
--- Content.Server/PDA/PdaSystem.cs
+++ Content.Server/PDA/PdaSystem.cs
@@ -19,6 +19,7 @@ using Content.Shared.Light;
 using Content.Shared.Light.EntitySystems;
 using Content.Shared.PDA;
 using Content.Shared.PDA.Ringer;
+using Content.Shared.VoiceMask;
 using Robust.Server.Containers;
 using Robust.Server.GameObjects;
 using Robust.Shared.Configuration; // DeltaV - PDA date
@@ -66,6 +67,8 @@ namespace Content.Server.PDA
             SubscribeLocalEvent<StationRenamedEvent>(OnStationRenamed);
             SubscribeLocalEvent<EntityRenamedEvent>(OnEntityRenamed, after: new[] { typeof(IdCardSystem) });
             SubscribeLocalEvent<AlertLevelChangedEvent>(OnAlertLevelChanged);
+            SubscribeLocalEvent<PdaComponent, InventoryRelayedEvent<ChameleonControllerOutfitSelectedEvent>>(OnRelayedEventToIdCard);
+            SubscribeLocalEvent<PdaComponent, InventoryRelayedEvent<VoiceMaskNameUpdatedEvent>>(OnRelayedEventToIdCard);
             SubscribeLocalEvent<PdaComponent, InventoryRelayedEvent<ChameleonControllerOutfitSelectedEvent>>(ChameleonControllerOutfitItemSelected);
 
             // Begin DeltaV additions
@@ -76,7 +79,7 @@ namespace Content.Server.PDA
             // End DeltaV additions
         }
 
-        private void ChameleonControllerOutfitItemSelected(Entity<PdaComponent> ent, ref InventoryRelayedEvent<ChameleonControllerOutfitSelectedEvent> args)
+        private void OnRelayedEventToIdCard<T>(Entity<PdaComponent> ent, ref InventoryRelayedEvent<T> args)
         {
             // Relay it to your ID so it can update as well.
             if (ent.Comp.ContainedId != null)
